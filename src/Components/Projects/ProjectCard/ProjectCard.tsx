5a29fcb2d63aa284527e150c16420afe9279a913
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-26 08:15:31 +0545

    Projects added

--- src/Components/Projects/ProjectCard/ProjectCard.tsx
+++ src/Components/Projects/ProjectCard/ProjectCard.tsx
@@ -1,40 +1,53 @@
 import { FaGithub } from 'react-icons/fa'
 import './ProjectCard.css'
+const ProjectCard = (props: {
+    title: string,
+    language: string,
+    propsImage: string,
+    hostLink?: string, // Make hostLink optional
+    githubLink: string[],
+    list: string[]
+}) => {
 
-const ProjectCard = (props: { title: string, language: string, propsImage: string, githubLink: string, list: string[] }) => {
-    const { title, language, githubLink, list, propsImage } = props
+    const { title, language, githubLink, list, propsImage, hostLink = githubLink[0] } = props
     return (
-        <div className="flip-card"
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            data-aos-once="true">
+        <a href={hostLink} target="_blank">
+            <div className="flip-card"
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                data-aos-once="true">
 
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <div className="flip-card-front-img-container">
-                        <img src={propsImage} alt="Prj 1" />
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <div className="flip-card-front-img-container">
+                            <img src={propsImage} alt="Prj 1" />
+                        </div>
+                        <div className='overlay-text'>{title}</div>
                     </div>
-                    <div className='overlay-text'>{title}</div>
-                </div>
-                <div className="flip-card-back">
-                    <h2>{title}</h2>
-                    <h3>{language}</h3>
-                    <ul>
-                        {
-                            list.map((data, index) => (
-                                <li key={index}>{data}</li>
-                            ))
-                        }
-                    </ul>
-                    <h2 className='github-link'>
-                        <a href={githubLink} target="_blank">
-                            <FaGithub />
-                        </a>
-                    </h2>
+                    <div className="flip-card-back">
+                        <h2>{title}</h2>
+                        <h3>{language}</h3>
+                        <ul>
+                            {
+                                list.map((data, index) => (
+                                    <li key={index}>{data}</li>
+                                ))
+                            }
+                        </ul>
+                        <h2 className='github-link'>
+                            {
+                                githubLink.map((data, index) => (
+                                    <a key={index} href={data} target="_blank">
+                                        <FaGithub />
+                                    </a>
+                                ))
+                            }
+                        </h2>
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     )
 }
 
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -14,16 +14,43 @@ const Projects = () => {
             {/* <p id='project-desc'>Some of my Solo Projects.</p> */}
 
             <div className="projects">
+
+                <ProjectCard
+                    title={"E-Commerce"}
+                    language={"React Node Express Typescript Postgres"}
+                    propsImage={image?.ecommerce}
+                    githubLink={["https://github.com/aashu1tosh/ecommerce--backend"]}
+                    list={["Full Stack PERN Application",
+                        "Implemented user authentication, tokenization",
+                        "Utilized Postgres for database management with TypeORM and Git for version control.",
+                        "Understood passing data through headers",
+                        "Developed this with enhance backend skills"
+                    ]}
+                />
+
+                <ProjectCard
+                    title={"SiteMark"}
+                    language={"React Node Express Typescript Postgres"}
+                    propsImage={image?.sitemark}
+                    githubLink={["https://github.com/aashu1tosh/project-0--frontend", "https://github.com/aashu1tosh/project-0--backend"]}
+                    list={["Full Stack PERN Application",
+                        "Implemented user authentication, tokenization, login, form validation and conditional rendering.",
+                        "Utilized MongoDB for database management and Git for version control.",
+                        "Understood passing data through headers",
+                        "Developed this with enhance frontend skills"
+                    ]}
+                />
+
                 <ProjectCard
                     title={"Annata Auto's (Classic Car Buying Selling Website)"}
-                    language={"React Typescript"}
+                    language={"React Node Express Typescript MongoDB"}
                     propsImage={image?.auto}
-                    githubLink={"https://github.com/aashu1tosh/annata-Vechile-Buying-Selling-Website-"}
+                    githubLink={["https://github.com/aashu1tosh/annata-Vechile-Buying-Selling-Website-"]}
                     list={["Full Stack MERN Application",
                         "Implemented user authentication, tokenization, role-based login and conditional rendering.",
                         "Utilized MongoDB for database management and Git for version control.",
                         "Understood passing data through headers",
-                        "Devloped this with enhance backendskills"
+                        "Developed this to enhance backend skills"
                     ]}
                 />
 
@@ -31,29 +58,33 @@ const Projects = () => {
                     title={"Youtube Clone"}
                     language={"React JSX"}
                     propsImage={image?.youtubeClone}
-                    githubLink={"https://github.com/aashu1tosh/youtube-clone"}
+                    githubLink={["https://github.com/aashu1tosh/youtube-clone"]}
                     list={["Integrated google’s open youtube api",
                         "Is responsive, beautiful and I learned CSS concepts during this project"]}
+                    hostLink='https://youtube.aashutoshparajuli.com.np/'
                 />
 
                 <ProjectCard
                     title={"News Application"}
                     language={"React Typescript"}
                     propsImage={image?.newsApp}
-                    githubLink={"https://github.com/aashu1tosh/news-app"}
+                    githubLink={["https://github.com/aashu1tosh/news-app"]}
                     list={[
-                        "Integrated newsapi.orgs api",
+                        "Integrated news api.orgs api",
                         "Is responsive and beautiful with Dark Light Theme toggle option."]}
+                    hostLink='https://news.aashutoshparajuli.com.np/'
                 />
 
                 <ProjectCard
                     title={"Weather Application"}
                     language={"React Typescript"}
                     propsImage={image?.weatherapp}
-                    githubLink={"https://github.com/aashu1tosh/my-weather-app"}
+                    githubLink={["https://github.com/aashu1tosh/my-weather-app"]}
                     list={["Integrated a openweathermap’s api",
                         "Multi Language Support (English and Nepali) where I used createContext and useContext hook.",
                         "Is responsive and beautiful with Dark Light Theme toggle option."]}
+                    hostLink='https://weather.aashutoshparajuli.com.np/'
+
                 />
 
             </div>
